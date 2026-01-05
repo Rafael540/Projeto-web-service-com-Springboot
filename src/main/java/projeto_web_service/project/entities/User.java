36c49703c794ff16c0f0d8b5251369ecbb452b0c
Rafael540 <alves540@hotmail.com>
2026-01-05 12:17:51 -0300

    hdatabase, test profile, JPA

--- src/main/java/projeto_web_service/project/entities/User.java
+++ src/main/java/projeto_web_service/project/entities/User.java
@@ -1,18 +1,23 @@
 package projeto_web_service.project.entities;
 
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serial;
 import java.io.Serializable;
-import java.util.Objects;
+
 
 @Getter
 @Setter
 @EqualsAndHashCode(of ="id")
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
-    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
