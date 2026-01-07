07a1284fff46a64c04ec3e8fbe365e268b46d524
Rafael540 <alves540@hotmail.com>
2026-01-07 14:11:28 -0300

    OrderStaus implementado

--- src/main/java/projeto_web_service/project/entities/Order.java
+++ src/main/java/projeto_web_service/project/entities/Order.java
@@ -3,8 +3,7 @@ package projeto_web_service.project.entities;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import projeto_web_service.project.entities.enums.OrderStatus;
 
 
 import java.io.Serializable;
@@ -24,16 +23,21 @@ public class Order  implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order() {}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
+
+
     }
 
     public Long getId() {
@@ -52,6 +56,16 @@ public class Order  implements Serializable {
         this.moment = moment;
     }
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
     public User getClient() {
         return client;
     }
@@ -59,4 +73,6 @@ public class Order  implements Serializable {
     public void setClient(User client) {
         this.client = client;
     }
+
+
 }
