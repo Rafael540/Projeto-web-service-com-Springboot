fe4f3ba84cb65d1055d9e8ec1d488964a672dffd
Rafael540 <alves540@hotmail.com>
2026-01-08 15:00:10 -0300

    OrderItem pt 1

--- src/main/java/projeto_web_service/project/entities/User.java
+++ src/main/java/projeto_web_service/project/entities/User.java
@@ -16,6 +16,7 @@ import java.util.List;
 @Table(name = "tb_users")
 @EqualsAndHashCode(of ="id")
 public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
 
 
     @Id
