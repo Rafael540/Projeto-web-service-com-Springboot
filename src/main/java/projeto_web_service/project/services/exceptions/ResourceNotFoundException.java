cab327719d32fd776f59c5b3b73e7b3f124c67e6
Rafael540 <alves540@hotmail.com>
2026-01-13 14:43:15 -0300

    Exceptions

---
+++ src/main/java/projeto_web_service/project/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package projeto_web_service.project.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found.Id " + id);
+    }
+}
