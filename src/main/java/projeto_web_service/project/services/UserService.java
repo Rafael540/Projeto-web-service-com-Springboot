bbf2b4ecac89a10b1bacd297f6ce06919513f56b
Rafael540 <alves540@hotmail.com>
2026-01-13 14:56:59 -0300

    Exception handler delete

--- src/main/java/projeto_web_service/project/services/UserService.java
+++ src/main/java/projeto_web_service/project/services/UserService.java
@@ -1,9 +1,12 @@
 package projeto_web_service.project.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import projeto_web_service.project.entities.User;
 import projeto_web_service.project.repositories.UserRepository;
+import projeto_web_service.project.services.exceptions.DatabaseException;
 import projeto_web_service.project.services.exceptions.ResourceNotFoundException;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+           throw new ResourceNotFoundException(id);
+        } catch(DataIntegrityViolationException e){
+           throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
